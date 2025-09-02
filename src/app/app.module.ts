bc60f731ed2b44d02d2050a504b725297c9d46b4
Marta Dashko <mda@terranoha.com>
2025-09-02 08:54:21 +0300

    update agular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,18 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { TrainListComponent } from './components/train-list/train-list.component';
 import { TrainDetailsComponent } from './components/train-details/train-details.component';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent,
         TrainListComponent,
         TrainDetailsComponent
     ],
-    imports: [
-        BrowserModule,
+    bootstrap: [AppComponent], imports: [BrowserModule,
         AppRoutingModule,
-        ReactiveFormsModule,
-        HttpClientModule
-    ],
-    bootstrap: [AppComponent]
-})
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
 
